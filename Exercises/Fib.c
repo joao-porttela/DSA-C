46b01601ecfae64987c9ba6536dc63d6ad61679d
joao-porttela <joao.portela1901@gmail.com>
2024-09-04 17:37:00 -0300

    Fibonacci recursion

---
+++ Exercises/Fib.c
@@ -0,0 +1,14 @@
+int Fib(int n)
+{
+	/*
+	 * n			0	1	2	3	4	5	6	7
+	 * fib(n)		0	1	1	2	3	5	8	13
+	 *
+	 * fib(n) = fib(n - 1) + gib(n - 2);
+	 */
+
+	if (n == 0 || n == 1 || n == 2)
+		return 1;
+
+	return Fib(n - 1) + Fib(n - 2);
+}
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "./Exercises/Fib.c"
 
 int main()
 {
+	static int n;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+
+	int a = Fib(n);
+	printf("%d", a);
+
 	return 0;
 };
